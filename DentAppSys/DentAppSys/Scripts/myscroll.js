2128edb145208e9afb3132f227f8ad73293a0841
templeofair <hr.laleh@gmail.com>
2014-04-17 15:27:22 +0200

    MenuIsDone!

---
+++ DentAppSys/DentAppSys/Scripts/myscroll.js
@@ -0,0 +1,34 @@
+﻿$.fn.scrollView = function () {
+    return this.each(function () {
+      var Mypos = $(this).offset().top - 250;
+      $('html, body').animate({
+        scrollTop: Mypos 
+      }, 1000);
+    });
+  }
+
+
+$('#sc1').click(function () {
+  
+  $('#service-1th').scrollView();
+});
+
+$('#sc2').click(function () {
+  
+  $('#service-2th').scrollView();
+});
+
+$('#sc3').click(function () {
+  
+  $('#service-3th').scrollView();
+});
+
+$('#sc4').click(function () {
+  
+  $('#service-4th').scrollView();
+});
+
+$('#sc5').click(function () {
+  
+  $('#service-5th').scrollView();
+});
