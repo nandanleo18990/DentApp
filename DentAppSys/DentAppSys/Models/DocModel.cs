84ad55e8b66d6f7c6b264f59b1895b1a32602c98
templeofair <hr.laleh@gmail.com>
2014-04-30 11:35:54 +0200

    DocumentFiles-GoingOn!

---
+++ DentAppSys/DentAppSys/Models/DocModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DentAppSys.Models
+{
+    public class GetDoc
+    {
+        [Display(Name = "Appointment ID = ")]
+        public int AppID { get; set; }
+        public int PatientID { get; set; }
+        [Display(Name = "Date From = ")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        [Display(Name = "To  = ")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+    }
+
+    public class ResultDoc
+    {
+        [Display(Name = "Appointment ID = ")]
+        public int AppID { get; set; }
+        [Display(Name = " Patient ID = ")]
+        public int PatientID { get; set; }
+        [Display(Name = "Date = ")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        [Display(Name = "Prescription = ")]
+        public string Prescription { get; set; }
+        [Display(Name = "Treatments Details = ")]
+        public string TreatmentDis { get; set; }
+        [Display(Name = "Status = ")]
+        public string Status { get; set; }
+        [Display(Name = "Related Image = ")]
+        public byte[] Image { get; set; }
+    }
+}
